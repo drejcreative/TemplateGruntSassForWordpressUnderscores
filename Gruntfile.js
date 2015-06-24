5a175b5342263e73ba5fc297a9ec1dfb204e404a
Dart <drejcreative@gmail.com>
2015-06-24 17:12:14 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig ({
+
+      pkg: grunt.file.readJSON('package.JSON'),
+
+      //sass task
+      sass: {
+        dev: {
+          options: {
+            style: 'expanded', //nested, compressed, expanded, compact
+            sourcemap: 'none',
+          },//options
+          files: {
+            'compiled/style-NONcompresed.css': 'sass/style.scss'
+          }//files
+        },//dev
+
+        dist: {
+          options: {
+            style: 'compressed', //nested, compressed, expanded, compact
+            sourcemap: 'none'
+          },//options
+          files: {
+            'compiled/style.css': 'sass/style.scss'
+          }//files
+        }//dist
+
+      },//sass
+
+      //Autoprefixer task
+      autoprefixer: {
+        options: {
+          browsers: ['last 2 versions']
+        },//options
+        multiple_files: {
+          expand: true,
+          flatten: true,
+          src: 'compiles/*.css',
+          dest: ''
+        }
+      },
+
+      //watch task
+      watch: {
+        css: {
+          files: '**/*.scss',
+          tasks: ['sass', 'autoprefixer']
+        }//css
+      }//watch
+
+  });//grunt.initConfig
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+  grunt.registerTask('default', ['watch']);
+
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Template for Wordpress workflow with grunt, Git and Sass, Compass and Susy responsivce framework
+
+![Template for Wordpress workflow with grunt, Git and Sass, Compass and Susy responsivce framewor](img.jpg)
+
+This is my template for developing  Wordpress theme with Sass and runing Grunt task runner for compiling scss into css. Grunt compile your Sass into css, and use autoprefixer to add browser prefixes to your css. Then compress your Sass file for lower file size and faster load time. We use underscore starter theme for building custom wordpress themes. 
+
+##Instructions
+
+Make sure you have these installed
+
+1. [Node.js](hwww.nodejs.org).
+2. [git](www.git-scm.com).
+3. [grunt](www.gruntjs.com).
+
+Clone this repository into your local machine using the terminal (mac) or Gitbash (PC)
+`git clone  https://github.com/drejcreative/Template-for-Grunt-workflow-with-Git-Sass-and-Compass.git`
+
+CD to the folder with workflows
+Run > `npm-install` to install the project dependencies
+
+Install grunt.js via the Mac terminal or Gitbash on a PC > `npm install -g grunt-cli`
+
+Run the Grunt command > `grunt`
+
+And open index.html in your browser and you are set to go
